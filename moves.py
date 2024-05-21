464d8b2849279a72a465fed837919f312d0767d6
Gfcarter2023 <gfcarter2023@gmail.com>
2024-05-21 16:50:28 -0400

    Setup

---
+++ moves.py
@@ -0,0 +1,37 @@
+import pyautogui
+
+def moveLeft():
+    return True
+
+def moveRight():
+    return True
+
+def moveUo():
+    return True
+
+def moveDown():
+    return True
+
+def jump():
+    return True
+
+def dash():
+    return True
+
+def grab():
+    return True
+
+def moveLeft():
+    return True
+
+def moveLeft():
+    return True
+
+def moveLeft():
+    return True
+
+def moveLeft():
+    return True
+
+def moveLeft():
+    return True
